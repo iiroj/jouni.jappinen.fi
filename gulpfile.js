aaf61d54cfdd8ad2fba06c9791ad104bb55f39b7
Iiro Jäppinen <iiro@jappinen.fi>
2016-05-07 14:55:03 +0300

    add ikuiset seprat page

--- gulpfile.js
+++ gulpfile.js
@@ -1,37 +1,44 @@
-var gulp         = require('gulp'),
-    sass         = require('gulp-sass'),
-    autoprefixer = require('gulp-autoprefixer'),
-    minifycss    = require('gulp-minify-css'),
-    base64       = require('gulp-base64'),
-    rename       = require('gulp-rename'),
-    concat       = require('gulp-concat'),
-    shell        = require('gulp-shell')
+const gulp         = require('gulp');
+const sass         = require('gulp-sass');
+const autoprefixer = require('gulp-autoprefixer');
+const cleanCSS     = require('gulp-clean-css');
+const rename       = require('gulp-rename');
+const webserver    = require('gulp-webserver');
 
 gulp.task('styles', function() {
-  return gulp.src('_styles/*.scss')
+  return gulp.src('_styles/*')
     .pipe(sass())
-    .pipe(autoprefixer('last 2 version', 'safari 5', 'ie 9', 'opera 12.1', 'ios 6', 'android 4'))
-    .pipe(minifycss())
-    .pipe(gulp.dest('_static'))
-})
+    .pipe(autoprefixer('last 2 version'))
+    .pipe(cleanCSS())
+    .pipe(gulp.dest('_static'));
+});
 
 gulp.task('static', ['styles'], function() {
   gulp.src(['_static/*'])
-    .pipe(gulp.dest('public'))
+    .pipe(gulp.dest('public'));
   gulp.src(['_media/*'])
-    .pipe(gulp.dest('public'))
-})
-
-gulp.task('devd', shell.task('devd -ol public/ \ /cv=http://devd.io:8000/cv.html \ /pajanhamarasta=http://devd.io:8000/pajanhamarasta.html'))
+    .pipe(gulp.dest('public'));
+});
 
 gulp.task('watch', function() {
-  gulp.watch('_static/*', ['static'])
-  gulp.watch('_styles/*.scss', ['static'])
-  gulp.watch('_components/*.html', ['components'])
-})
+  gulp.watch('_static/*', ['static']);
+  gulp.watch('_styles/*.scss', ['static']);
+});
+
+gulp.task('webserver', function() {
+  gulp.src('public')
+    .pipe(webserver({
+      directoryListing: false,
+      fallback: 'index.html',
+      host: 'localhost',
+      livereload: true,
+      open: true,
+      port: 8080
+    }));
+});
 
 gulp.task('default', function() {
-  gulp.start('static')
-  gulp.start('watch')
-  gulp.start('devd')
-})
+  gulp.start('static');
+  gulp.start('watch');
+  gulp.start('webserver');
+});
