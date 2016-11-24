09249540089e43c555f7d0bdc9898687e8d7bcd7
Iiro Jäppinen <iiro@jappinen.fi>
2016-11-24 12:05:29 +0200

    use Link and fix linting issues

--- pages/ikuisetseprat/index.js
+++ pages/ikuisetseprat/index.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import { Link } from 'react-router'
 import Helmet from 'react-helmet'
 
 import { config } from 'config'
@@ -10,11 +11,11 @@ export default class Ikuisetseprat extends Component {
     return (
       <div className='ikuisetsepratPage'>
         <Helmet title={`Ikuiset seprat – ${config.authorName}`} />
-        <main typeof="Book">
-          <div className="image"></div>
+        <main>
+          <div className='image' />
           <header>
-            <h1 property="headline">Ikuiset seprat</h1>
-            <h2 property="alternativeHeadline">Tytärsaarelaisten ja virolaisten suhteet ennen toista maailmansotaa</h2>
+            <h1>Ikuiset seprat</h1>
+            <h2>Tytärsaarelaisten ja virolaisten suhteet ennen toista maailmansotaa</h2>
           </header>
           <section>
             <p>Tytärsaaren perifeerinen sijainti lähempänä Viron kuin Suomen rannikkoa tarjosi saarelaisille mahdollisuuden ylläpitää vilkkaita suhteita virolaisten kanssa satojen vuosien ajan. Elämä Tytärsaarella oli fyysisesti raskasta ja edellytti saarelaiselta täydellistä omatoimisuutta ja monitaituruutta. Elinehto oli virolaisten kanssa käyty suolasilakan vaihtokauppa maanviljelystuotteisiin. Kanssakäyminen edellytti saarelaisilta laivanrakennus- ja merenkulkutaitoa, sekä viron kielen osaamista. Satojen vuosien kuluessa kielet ja tavat sekoittuivat osittain toisiinsa. Virolaiset ja tytärsaarelaiset pitivät toisiaan avoimina ja rehellisinä. Virossa tytärsaarelaisten ei muisteta pettäneen virolaisia ystäviään kertaakaan.</p>
@@ -22,13 +23,12 @@ export default class Ikuisetseprat extends Component {
             <p>Tytärsaaren nuorisoseura aloitti 1920-luvun lopulla esiintymismatkat Viroon, jotka huipentuivat vuosina 1935– 1939 kahteentoista suureen heimojuhlaan Tytärsaaressa sekä Purtse-Jõesuun, Lüganusen, Pikkaristin, Sämin ja Varudin kylissä Virumaalla. Suurimpiin juhliin osallistui Sämisillan kievarissa yli tuhat virolaista vierasta. Tytärsaaren nuorisoseuran huvitoimikunta esitti tuolloin virolaisille useita näytelmiä, kansantanssia ja runonlausuntaa.</p>
             <p>Vilkas kanssakäyminen johti 1920- ja 1930-luvulla lukuisiin seurustelusuhteisiin, kirjeenvaihtoystävyyteen ja lopulta neljään avioliittoon, tytärsaarelaisten kalastajien ja laivurien löytäessä Virumaalta morsiamen. Seprojen ystävyyssuhteet katkesivat äkillisesti 11.10.1939, jolloin Tytärsaari evakuoitiin muutaman tunnin kuluessa. Tuona päivänä Virossa oli useita tytärsaarelaisia aluksineen, joille evakuointikäsky vietiin moottoriveneellä ja jolloin ystävyyssuhteet katkesivat.</p>
           </section>
-          <a href="http://www.kymensanomat.fi/Online/2016/06/21/Kirja%3A%20Yst%C3%A4vyytt%C3%A4%20%20yli%20merien/2016320953078/4" rel="external crossorigin prefetch" property="url" target="_blank">Lue kirja-arvostelu</a>
-          <a href="http://www.adlibris.com/fi/kirja/ikuiset-seprat-9789529371310" rel="external crossorigin prefetch" property="url" target="_blank">Osta Adlibris-verkkokirjakaupasta</a>
+          <a href='http://www.kymensanomat.fi/Online/2016/06/21/Kirja%3A%20Yst%C3%A4vyytt%C3%A4%20%20yli%20merien/2016320953078/4' rel='external crossorigin prefetch'>Lue kirja-arvostelu</a>
+          <a href='http://www.adlibris.com/fi/kirja/ikuiset-seprat-9789529371310' rel='external crossorigin prefetch'>Osta Adlibris-verkkokirjakaupasta</a>
           <footer>
-            <p property="author">Jouni Jäppinen</p>
-            <p property="isbn">ISBN 978-952-93-7131-0</p><a href="/" rel="prefetch">Takaisin</a>
-            <meta property="inLanguage" content="fi-FI"/>
-            <meta property="publisher" content="Books on Demand"/>
+            <p>Jouni Jäppinen</p>
+            <p>ISBN 978-952-93-7131-0</p>
+            <Link to='/'>Takaisin</Link>
           </footer>
         </main>
       </div>
--- pages/pajanhamarasta/index.js
+++ pages/pajanhamarasta/index.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import { Link } from 'react-router'
 import Helmet from 'react-helmet'
 
 import { config } from 'config'
@@ -10,11 +11,11 @@ export default class Pajanhamarasta extends Component {
     return (
       <div className='pajanhamarastaPage'>
         <Helmet title={`Pajan hämärästä – ${config.authorName}`} />
-        <main typeof="Book">
-          <div className="image"></div>
+        <main>
+          <div className='image' />
           <header>
-            <h1 property="headline">Pajan hämärästä</h1>
-            <h2 property="alternativeHeadline">Sepän kulttuurihistoriaa</h2>
+            <h1>Pajan hämärästä</h1>
+            <h2>Sepän kulttuurihistoriaa</h2>
           </header>
           <section>
             <p>Vanhan sanonnan mukaan seppä on taitava henkilö niin aineen kuin hengenkin alalla. Kirja luo katsauksen sepänalan esihistoriaan sekä tarinoihin kotimaamme sepistä, joita on onnekkaasti säilynyt jälkipolville. Muistitiedon mukaan seppien joukkoon mahtui monenlaisia persoonallisuuksia ja osa heistä toimi pajatyön ohella monenlaisissa muissakin tehtävissä, kuten kylänvanhimpina, poliitikkoina, parantajina, verenlaskijoina, tai -seisauttajina, näkijöinä, noitina, hautausurakoitsijoina, soitinrakentajina, puoskareina ja jopa kätilöinä.</p>
@@ -23,13 +24,12 @@ export default class Pajanhamarasta extends Component {
             <p>Yhteisön arvomaailman ja normien halveksiminen tuli esille mitä moninaisimimpien tempausten muodossa. Elämää väritti ennen vanhaan taikausko, noituus ja magia ja sepillä oli niiden harjoittamisessa oma tärkeä roolinsa, ensisijaisesti pajaelinkeinon edistäjänä, joka lienee vanhaa perua rautakaudelta. Taikuuden harjoittaminen pajassa pönkitti sepän asemaa kyläyhteisössä ja samalla se ylläpiti ammoisista ajoista asti periytynyttä sankarimyyttiä.</p>
             <p>Sepät eivät paljastaneet salaisuuksiaan muille ja se sai ihmiset äärimmäisen uteliaiksi. Kun kyläläiset eivät ymmärtäneet sepän käyttämien prosessien kulkua ja kun seppä tuntui alati keskustelevan ahjon, alasimen tai palkeiden kanssa, alkoivat ihmiset uskoa, että sepällä täytyi olla luontaisia kykyjä hallita yliluonnollisia voimia.</p>
           </section>
-          <a href="/pajanhamarasta-arvostelu.jpg" rel="prefetch" property="url" target="_blank">Lue kirja-arvostelu</a>
-          <a href="https://www.adlibris.com/fi/kirja/pajan-hamarasta-9789529342501" rel="external crossorigin prefetch" property="url" target="_blank">Osta Adlibris-verkkokirjakaupasta</a>
+          <a href='/pajanhamarasta-arvostelu.jpg' rel='prefetch'>Lue kirja-arvostelu</a>
+          <a href='https://www.adlibris.com/fi/kirja/pajan-hamarasta-9789529342501' rel='external crossorigin prefetch'>Osta Adlibris-verkkokirjakaupasta</a>
           <footer>
-            <p property="author">Jouni Jäppinen</p>
-            <p property="isbn">ISBN-978-952-93-4250-1</p><a href="/" rel="prefetch">Takaisin</a>
-            <meta property="inLanguage" content="fi-FI"/>
-            <meta property="publisher" content="Books on Demand"/>
+            <p>Jouni Jäppinen</p>
+            <p>ISBN-978-952-93-4250-1</p>
+            <Link to='/'>Takaisin</Link>
           </footer>
         </main>
       </div>
