736632fcbb893872305edf46622e01987d53ff2d
Iiro Jäppinen <iiro@jappinen.fi>
2018-08-06 08:02:52 +0300

    Add netlify plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,3 +1,3 @@
 module.exports = {
-  plugins: ["gatsby-plugin-emotion", "gatsby-plugin-react-helmet"],
+  plugins: ["gatsby-plugin-emotion", "gatsby-plugin-react-helmet", "gatsby-plugin-netlify"],
 };
