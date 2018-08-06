8ad06d6df68d35b7ec418539253cf9b8a9a05df2
Iiro Jäppinen <iiro@jappinen.fi>
2018-08-06 08:04:25 +0300

    Add offline plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,3 +1,3 @@
 module.exports = {
-  plugins: ["gatsby-plugin-emotion", "gatsby-plugin-react-helmet", "gatsby-plugin-netlify"],
+  plugins: ["gatsby-plugin-emotion", "gatsby-plugin-react-helmet", "gatsby-plugin-offline", "gatsby-plugin-netlify"],
 };
