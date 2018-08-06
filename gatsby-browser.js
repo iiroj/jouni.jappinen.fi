0405a4a745a93477d53434639263ea8b1ff16d8e
Iiro Jäppinen <iiro@jappinen.fi>
2018-08-06 12:36:14 +0300

    Fix case-sensitivity

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-import Layout from "./src/components/layout";
+import Layout from "./src/components/Layout";
 
 export const wrapRootComponent = ({ Root }) => () => (
   <Layout>
