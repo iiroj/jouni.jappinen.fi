c547694ac34f0c7f38aeef42ed87b5cae01cfa52
Iiro Jäppinen <iiro@jappinen.fi>
2018-08-09 10:09:40 +0300

    Add font preload and remove caching headers

--- gatsby-config.js
+++ gatsby-config.js
@@ -7,13 +7,18 @@ module.exports = {
       options: {
         headers: {
           '/*': [
-            'Cache-Control: public, max-age=2592000, no-cache',
             "Content-Security-Policy: default-src 'none'; script-src 'self' 'unsafe-inline' 'unsafe-eval'; img-src 'self' data:; connect-src 'self' https://fonts.gstatic.com; style-src 'self' 'unsafe-inline' https://fonts.googleapis.com; font-src 'self' https://fonts.gstatic.com;",
             'Referrer-Policy: origin-when-cross-origin'
-          ],
-          '/sw.js': ['Cache-Control: max-age=0, no-cache, no-store, must-revalidate', 'Expires: 0', 'Pragma: no-cache']
+          ]
         },
-        mergeCachingHeaders: false
+        mergeCachingHeaders: false,
+        transformHeaders: (headers, path) =>
+          path.endsWith('/') || path.endsWith('.html')
+            ? headers.concat(
+                'Link: <https://fonts.gstatic.com/s/spectral/v4/rnCr-xNNww_2s0amA9M5knjsS_ul.woff2>; rel=preload; as=font',
+                'Link: <https://fonts.gstatic.com/s/spectral/v4/rnCs-xNNww_2s0amA9vmtm3BafaPWnII.woff2>; rel=preload; as=font'
+              )
+            : headers
       }
     }
   ]
