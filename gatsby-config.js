60be13474dfec38b9d38701aac67b2e792d780e7
Iiro Jäppinen <iiro@jappinen.fi>
2018-10-20 14:12:16 +0300

    ci: deploy site to AWS with discharge

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,34 +1,3 @@
-const cspConfig = [
-  "connect-src 'self' https://fonts.gstatic.com;",
-  "default-src 'none';",
-  "font-src 'self' https://fonts.gstatic.com;",
-  "img-src 'self' data: https://*.cloudfront.net;",
-  "script-src 'self' 'unsafe-inline' 'unsafe-eval';",
-  "style-src 'self' 'unsafe-inline' https://fonts.googleapis.com;"
-];
-
 module.exports = {
-  plugins: [
-    'gatsby-plugin-react-helmet',
-    {
-      resolve: 'gatsby-plugin-netlify',
-      options: {
-        headers: {
-          '/*': [
-            `Content-Security-Policy: ${cspConfig.join(' ')}`,
-            "Feature-Policy: default 'none'",
-            'Referrer-Policy: no-referrer-when-downgrade'
-          ]
-        },
-        mergeCachingHeaders: false,
-        transformHeaders: (headers, path) =>
-          path.endsWith('/') || path.endsWith('.html')
-            ? headers.concat(
-                'Link: <https://fonts.gstatic.com/s/spectral/v4/rnCr-xNNww_2s0amA9M5knjsS_ul.woff2>; rel=preload; as=font',
-                'Link: <https://fonts.gstatic.com/s/spectral/v4/rnCs-xNNww_2s0amA9vmtm3BafaPWnII.woff2>; rel=preload; as=font'
-              )
-            : headers
-      }
-    }
-  ]
+  plugins: ['gatsby-plugin-react-helmet']
 };
