bf5c9e4ea0991d91b9f64424946d66e7afe78d71
Iiro Jäppinen <iiro@jappinen.fi>
2019-05-01 14:03:11 +0300

    feat: deploy to Netlify

--- README.md
+++ README.md
@@ -1,5 +1,6 @@
 # jouni.jappinen.fi
 
+[![Netlify Status](https://api.netlify.com/api/v1/badges/c0c29251-2b92-4235-870e-797a6ec77613/deploy-status)](https://app.netlify.com/sites/jouni/deploys)
 [![version](https://img.shields.io/github/tag/iiroj/jouni.jappinen.fi.svg)](https://github.com/iiroj/jouni.jappinen.fi/releases)
 [![dependencies](https://img.shields.io/david/iiroj/jouni.jappinen.fi.svg)](https://github.com/iiroj/jouni.jappinen.fi/blob/master/package.json)
 [![devDependencies](https://img.shields.io/david/dev/iiroj/jouni.jappinen.fi.svg)](https://github.com/iiroj/jouni.jappinen.fi/blob/master/package.json)
--- gatsby-config.js
+++ gatsby-config.js
@@ -1,5 +1,15 @@
 const path = require("path");
 
+const cspConfig = [
+  "default-src 'none'",
+  "connect-src 'self' https://fonts.gstatic.com",
+  "font-src 'self' https://fonts.gstatic.com",
+  "img-src 'self' data: https://*.cloudfront.net",
+  "object-src 'self'",
+  "script-src 'self' 'unsafe-inline' 'unsafe-eval'",
+  "style-src 'self' 'unsafe-inline' https://fonts.googleapis.com"
+];
+
 module.exports = {
   plugins: [
     {
@@ -10,6 +20,24 @@ module.exports = {
       }
     },
     "gatsby-plugin-sharp",
-    "gatsby-transformer-sharp"
+    "gatsby-transformer-sharp",
+    "gatsby-transformer-sharp",
+    {
+      resolve: "gatsby-plugin-netlify",
+      options: {
+        headers: {
+          "/*": [
+            "Link: <https://fonts.googleapis.com/css?family=Spectral:400,600>;crossorigin=anonymous;rel=preload;as=style;",
+            "Referrer-Policy: no-referrer-when-downgrade",
+            "Referrer-Policy: strict-origin-when-cross-origin",
+            "Strict-Transport-Security: max-age=63072000; includeSubdomains; preload",
+            "X-Content-Type-Options: nosniff",
+            "X-Frame-Options: DENY",
+            "X-XSS-Protection: 1; mode=block",
+            `Content-Security-Policy: ${cspConfig.join("; ")}`
+          ]
+        }
+      }
+    }
   ]
 };
