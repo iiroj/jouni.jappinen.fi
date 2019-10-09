e587af1f2425bf266b5d025e0d053e9e75e0b7e4
Iiro Jäppinen <iiro@jappinen.fi>
2019-10-09 16:10:14 +0300

    build: use serverless target

--- next.config.js
+++ next.config.js
@@ -1,9 +1,1 @@
-module.exports = {
-  target: "server",
-  exportPathMap: async () => ({
-    "/": { page: "/" },
-    "/ikuiset-seprat": { page: "/ikuiset-seprat" },
-    "/pajan-hamarasta": { page: "/pajan-hamarasta" },
-    "/rautakymi": { page: "/rautakymi" }
-  })
-};
+module.exports = { target: "serverless" };
