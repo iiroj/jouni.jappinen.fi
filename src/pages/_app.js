561483c249eacc4cf4a12e7ebeb9a74eec907586
Iiro Jäppinen <iiro@jappinen.fi>
2020-10-31 09:54:43 +0200

    refactor: move static head tags to _app

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,3 +1,4 @@
+import Head from "next/head";
 import { HelmetProvider } from "react-helmet-async";
 import React from "react";
 
@@ -10,6 +11,18 @@ const App = ({ Component, pageProps }) => {
   return (
     <>
       <Reset />
+      <Head>
+        <link
+          crossOrigin="anonymous"
+          href="https://fonts.googleapis.com/css?family=Spectral:400,600&display=swap"
+          rel="stylesheet"
+        />
+        <link rel="icon" href="/favicon.ico" type="image/x-icon" />
+        <link rel="apple-touch-icon" href="/icon.png" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta name="theme-color" content="#000000" />
+        <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
+      </Head>
       <HelmetProvider context={helmet.current}>
         <Main>
           <Component {...pageProps} helmetContext={helmet.current} />
--- src/pages/_document.js
+++ src/pages/_document.js
@@ -31,16 +31,6 @@ class Document extends NextDocument {
               {helmetContext && helmetContext.helmet.script.toComponent()}
             </>
           )}
-          <link
-            crossOrigin="anonymous"
-            href="https://fonts.googleapis.com/css?family=Spectral:400,600&display=swap"
-            rel="stylesheet"
-          />
-          <link rel="icon" href="/favicon.ico" type="image/x-icon" />
-          <link rel="apple-touch-icon" href="/icon.png" />
-          <meta name="viewport" content="width=device-width, initial-scale=1" />
-          <meta name="theme-color" content="#000000" />
-          <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
         </Head>
         <body>
           <Main />
