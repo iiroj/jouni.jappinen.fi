bd32a7224f7c782cb111b36492593fdb64f039b9
Iiro Jäppinen <iiro@jappinen.fi>
2021-09-10 13:54:54 +0300

    build: enable esmExternals option

--- next.config.js
+++ next.config.js
@@ -3,6 +3,7 @@ module.exports = {
         CF_WEB_ANALYTICS_TOKEN: process.env.CF_WEB_ANALYTICS_TOKEN,
     },
     eslint: { ignoreDuringBuilds: true },
+    experimental: { esmExternals: true },
     reactStrictMode: true,
     target: 'serverless',
     trailingSlash: true,
