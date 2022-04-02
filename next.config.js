3ab6c963c62d2fa6da5784c3d54c9c7992f3207f
Iiro Jäppinen <iiro@jappinen.fi>
2022-04-02 10:51:22 +0300

    build: flatten repo structure

--- next.config.js
+++ next.config.js
@@ -4,7 +4,6 @@ const config = {
     experimental: {
         runtime: 'edge',
     },
-    exportPathMap: async (defaultPaths) => defaultPaths,
     headers,
     poweredByHeader: false,
     swcMinify: true,
