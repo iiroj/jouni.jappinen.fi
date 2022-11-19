46ddd211666d29671b625592481929663472f1c2
Iiro Jäppinen <iiro@jappinen.fi>
2022-11-19 11:43:01 +0200

    perf: use renderToReadableStream

--- app/entry.server.jsx
+++ app/entry.server.jsx
@@ -1,19 +1,39 @@
 import { RemixServer } from '@remix-run/react'
-import { renderToString } from 'react-dom/server'
+import { renderToReadableStream } from 'react-dom/server'
 import React from 'react'
 import { ServerStyleSheet } from 'styled-components'
 
-const handleRequest = (request, responseStatusCode, responseHeaders, remixContext) => {
-    const sheet = new ServerStyleSheet()
+// eslint-disable-next-line no-undef
+const textDecorer = new TextDecoderStream()
+// eslint-disable-next-line no-undef
+const textEncoder = new TextEncoderStream()
 
-    let markup = renderToString(sheet.collectStyles(<RemixServer context={remixContext} url={request.url} />))
+class StyleSheetInjector extends TransformStream {
+    constructor({ sheet }) {
+        const styles = sheet.getStyleTags()
+        super({
+            transform: (chunk, controller) => {
+                controller.enqueue(chunk.replace('__STYLES__', styles))
+            },
+        })
+    }
+}
 
-    const styles = sheet.getStyleTags()
-    markup = markup.replace('__STYLES__', styles)
-
+const handleRequest = async (request, responseStatusCode, responseHeaders, remixContext) => {
     responseHeaders.set('Content-Type', 'text/html')
 
-    return new Response(`<!DOCTYPE html>${markup}`, {
+    const sheet = new ServerStyleSheet()
+
+    const stream = await renderToReadableStream(
+        sheet.collectStyles(<RemixServer context={remixContext} url={request.url} />)
+    )
+
+    const transformed = stream
+        .pipeThrough(textDecorer)
+        .pipeThrough(new StyleSheetInjector({ sheet }))
+        .pipeThrough(textEncoder)
+
+    return new Response(transformed, {
         status: responseStatusCode,
         headers: responseHeaders,
     })
