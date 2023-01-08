64fdc9adb7ad4e6b54fd0e968df2929994fcb976
Iiro Jäppinen <iiro@jappinen.fi>
2023-01-08 17:10:30 +0200

    fix: definitely fix loading Cloudflare analytics script

--- app/root.jsx
+++ app/root.jsx
@@ -1,4 +1,4 @@
-import { Links, LiveReload, Meta, Outlet, Scripts, ScrollRestoration } from '@remix-run/react';
+import { Links, LiveReload, Meta, Outlet, Scripts, ScrollRestoration, useLoaderData } from '@remix-run/react';
 import React from 'react';
 
 import Reset from './components/Reset';
@@ -39,8 +39,10 @@ const App = () => {
 
                 {cfBeaconToken ? (
                     <script
+                        crossOrigin="anonymous"
                         data-cf-beacon={`{"token": "${cfBeaconToken}"}`}
                         defer
+                        integrity="sha384-ikHI8F+DCZ/OP4pxSzGmk/E2UZdZ8TNqbm/HJcr90E1vUeqCnW7y8D8dC9AGuWV9"
                         src="https://static.cloudflareinsights.com/beacon.min.js"
                     />
                 ) : null}
--- server/headers.js
+++ server/headers.js
@@ -3,42 +3,41 @@ const SECURITY_HEADERS = {
     'Content-Security-Policy': [
         `default-src 'self'`,
         `img-src 'self' data:`,
-        `script-src 'self' 'unsafe-inline' 'unsafe-eval'`,
         `style-src 'self' 'unsafe-inline'`,
-    ].join(';'),
+        process.env.NODE_ENV === 'production'
+            ? ['connect-src cloudflareinsights.com', `script-src 'self' 'unsafe-inline' static.cloudflareinsights.com`]
+            : [`script-src 'self' 'unsafe-inline'`],
+    ]
+        .flat()
+        .join(';'),
+
     /** @see https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Strict-Transport-Security */
     'Strict-Transport-Security': 'max-age=63072000; includeSubDomains; preload',
+
     /** @see https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/X-XSS-Protection */
     'X-XSS-Protection': '1; mode=block',
+
     /** @see https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/X-Frame-Options */
     'X-Frame-Options': 'DENY',
+
     /** @see https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/X-Content-Type-Options */
     'X-Content-Type-Options': 'nosniff',
+
     /** @see https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Referrer-Policy */
     'Referrer-Policy': 'strict-origin-when-cross-origin',
+
     /** @see https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Cross-Origin-Embedder-Policy */
     'Cross-Origin-Embedder-Policy': 'require-corp; report-to="default";',
+
     /** @see https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Cross-Origin-Opener-Policy */
     'Cross-Origin-Opener-Policy': 'same-site; report-to="default";',
+
     /** @see https://developer.mozilla.org/en-US/docs/Web/HTTP/Cross-Origin_Resource_Policy_(CORP) */
     'Cross-Origin-Resource-Policy': 'same-site',
 };
 
 export const getHeaders = (init) => {
     const headers = new Headers(init);
-
-    const isProduction = process.env.NODE_ENV === 'production';
-    if (isProduction) {
-        /** @see https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Content-Security-Policy */
-        headers.set(
-            'Content-Security-Policy',
-            [
-                `default-src 'self'`,
-                /** @see https://static.cloudflareinsights.com/beacon.min.js */
-                `script-src 'self' 'unsafe-inline' sha512-d1a843625f3ae943212ae61c5b4efc49c31a957aad1498209bb4e6954b69d14943e1e424d08c5f9e6e729172af1893458cb328a2bb5db1e4dfb114fca027e018`,
-            ].join(';'),
-        );
-    }
 
     for (const [headerName, headerValue] of Object.entries(SECURITY_HEADERS)) {
         headers.set(headerName, headerValue);
